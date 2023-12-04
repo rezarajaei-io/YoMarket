c75154dab6ae835d48c4f4128b6d083cbbbeeb04
Reza Rajaei <rajaeireza428@gmail.com>
2023-12-04 22:52:25 +0330

    show Admin Panel

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using YoMarket.Data;
+using YoMarket.Models;
 
 namespace YoMarket.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private EshopContext _context;
+        public IndexModel(EshopContext context)
+        {
+          _context = context;
+        }
+        public IEnumerable<Product> Products { get; set; }
         public void OnGet()
         {
+            Products = _context.Products.Include(p => p.Item);
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
